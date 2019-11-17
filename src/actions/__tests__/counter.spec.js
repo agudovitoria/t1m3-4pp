d5172a0204c5edf8263793c89fc5425c2e52a6a6
Iñaki Agudo <agudovitoria@gmail.com>
2019-11-17 23:54:58 +0100

    Remove unnecessary exaple test

--- src/actions/__tests__/counter.spec.js
+++ src/actions/__tests__/counter.spec.js
@@ -1,25 +1,26 @@
-import { spy } from 'sinon';
-import { increment, decrement, incrementAsync } from '../../src/actions/counter';
+// import { spy } from 'sinon';
+// import { increment, decrement, incrementAsync } from '../../src/actions/counter';
+// import { describe, it, expect } from 'jest';
 
-describe('Counter __tests__', () => {
-  it('should create increment action', () => {
-    expect(increment()).toEqual({ type: increment.toString() });
-  });
+// describe('Counter __tests__', () => {
+//   it('should create increment action', () => {
+//     expect(increment()).toEqual({ type: increment.toString() });
+//   });
 
-  it('should create decrement action', () => {
-    expect(decrement()).toEqual({ type: decrement.toString() });
-  });
+//   it('should create decrement action', () => {
+//     expect(decrement()).toEqual({ type: decrement.toString() });
+//   });
 
-  it('should create increment action async', (done) => {
-    const fn = incrementAsync(1);
-    expect(fn).toBeInstanceOf(Function);
+//   it('should create increment action async', (done) => {
+//     const fn = incrementAsync(1);
+//     expect(fn).toBeInstanceOf(Function);
 
-    const dispatch = spy();
-    fn(dispatch);
+//     const dispatch = spy();
+//     fn(dispatch);
 
-    setTimeout(() => {
-      expect(dispatch.calledWith({ type: increment.toString() })).toEqual(true);
-      done();
-    }, 5);
-  });
-});
+//     setTimeout(() => {
+//       expect(dispatch.calledWith({ type: increment.toString() })).toEqual(true);
+//       done();
+//     }, 5);
+//   });
+// });
